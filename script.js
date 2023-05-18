7e8863ff123fbe64f108091b87d2328940ed5f3f
saurabh singh <86177535+saurabhsngh5484@users.noreply.github.com>
2023-05-18 10:20:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+const parent = document.querySelector("#parent");
+let start = null;
+
+parent.addEventListener("dragstart", (event) => {
+  start = event.target;
+});
+
+parent.addEventListener("dragover", (event) => {
+  event.preventDefault();
+});
+
+parent.addEventListener("drop", (event) => {
+  event.preventDefault();
+  const final = event.target;
+  const parent = final.parentNode;
+  if (final !== start) {
+    parent.insertBefore(start, final);
+  }
+});
